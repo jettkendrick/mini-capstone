1b467af392f88e21bf0c3ea00ddbca7cc70e0a33
jettkendrick <jettkendrick@gmail.com>
2018-11-01 20:54:20 -0500

    added more restful routes and links

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,42 @@
 class ProductsController < ApplicationController
   def index 
     @products = Product.all
+    @products = @products.order(:id => :asc)
     render "index.html.erb"
+  end 
+
+  def new 
+    render "new.html.erb"
   end 
 
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
+  end 
+
+  def create 
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price].to_i,
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+      )
+    @product.save
+    redirect_to "/products"
+  end 
+
+  def edit
+    @product = Product.find_by(id: params[:id]) 
+    render "edit.html.erb"
+  end 
+
+  def update 
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.supplier_id = params[:supplier_id] 
+    @product.save
+    redirect_to "/products"
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
   end
 
   get "/products" => "products#index"
+  get "products/new" => "products#new" 
   get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
 end
 
 
